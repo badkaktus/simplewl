ada401bace2d6426b66e3247c381bbdc2c45e0e3
badkaktus <kolyuchy@gmail.com>
2024-01-10 21:15:24 +0200

    update sh

--- .fly/scripts/1_storage_init.sh
+++ .fly/scripts/1_storage_init.sh
@@ -4,3 +4,12 @@ if [ ! -d "$FOLDER" ]; then
     mkdir /var/www/html/storage/database
     touch /var/www/html/storage/database/database.sqlite
 fi
+
+if [ -z "$RELEASE_COMMAND" ]; then
+    # We are NOT in a temporary VM, run as normal...
+    cd /var/www/html
+    php artisan migrate --force
+else
+    # We are in the temporary VM created
+    # for release commands...
+fi
