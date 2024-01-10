30431a381000b106c639353a728191cace5f1b15
badkaktus <kolyuchy@gmail.com>
2024-01-10 21:23:18 +0200

    check exist file in sh script

--- .fly/scripts/1_storage_init.sh
+++ .fly/scripts/1_storage_init.sh
@@ -1,8 +1,16 @@
+# .fly/scripts/1_storage_init.sh
+
+# Add this below the storage folder initialization snippet
 FOLDER=/var/www/html/storage/database
+DATABASE_FILE=$FOLDER/database.sqlite
+
 if [ ! -d "$FOLDER" ]; then
     echo "$FOLDER is not a directory, initializing database"
-    mkdir /var/www/html/storage/database
-    touch /var/www/html/storage/database/database.sqlite
+    mkdir "$FOLDER"
+    touch "$DATABASE_FILE"
+elif [ ! -f "$DATABASE_FILE" ]; then
+    echo "Creating $DATABASE_FILE in the existing directory"
+    touch "$DATABASE_FILE"
 fi
 
 if [ -z "$RELEASE_COMMAND" ]; then
