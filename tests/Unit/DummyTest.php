f177f34ec4f037ced44d917b46e99f825db8cb18
badkaktus <kolyuchy@gmail.com>
2024-01-14 15:36:17 +0200

    add dummy unit test

---
+++ tests/Unit/DummyTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class DummyTest extends TestCase
+{
+    public function test_dummy(): void
+    {
+        $this->assertTrue(true);
+    }
+}
