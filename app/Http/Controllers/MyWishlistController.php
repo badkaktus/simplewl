d501506a13c5c194b629433f93e10d3e8747949b
badkaktus <kolyuchy@gmail.com>
2024-01-21 18:00:55 +0200

    test fix

--- app/Http/Controllers/MyWishlistController.php
+++ app/Http/Controllers/MyWishlistController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\TryToOpenPrivateWishlist;
+use App\Models\Wishlist;
+use App\Services\WishlistService;
 use App\Services\WishService;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -10,20 +13,28 @@ use Illuminate\Support\Facades\Auth;
 
 class MyWishlistController extends Controller
 {
-    public function __construct(private readonly WishService $wishService)
-    {
+    public function __construct(
+        private readonly WishService $wishService,
+        private readonly WishlistService $wishlistService,
+    ) {
     }
 
+    /**
+     * @throws TryToOpenPrivateWishlist
+     */
     public function index(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
         $user = Auth::user();
+        // todo set slug, when custom wishlist was added
         $wishes = $this->wishService->getWishesByUserAndSlug($user->name, null);
+        $wishlist = $this->wishlistService->getWishlistByUserIdAndSlug($user->id, Wishlist::DEFAULT_WISHLIST_SLUG);
 
         return view(
             'wishlist.index',
             [
                 'wishes' => $wishes,
                 'user' => $user,
+                'wishlist' => $wishlist,
             ]
         );
     }
