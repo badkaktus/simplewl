6417ba2684ebb1ed9a40b8752e92498bb81660ab
badkaktus <kolyuchy@gmail.com>
2024-02-19 22:32:27 +0200

    remove comment part of code

--- resources/views/wish/show.blade.php
+++ resources/views/wish/show.blade.php
@@ -27,9 +27,6 @@
 
                                 @if(!$wish->is_completed)
                                     <div class="font-medium" x-data="{ showButton: true }">
-{{--                                        <form method="POST" action="{{ route('wish.complete',  $wish->slug) }}">--}}
-{{--                                            @csrf--}}
-{{--                                            <button type="submit"--}}
                                             <button
                                                     x-show="showButton"
                                                     @click="toggleCompleteState"
@@ -41,7 +38,6 @@
                                                         d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
                                                 </svg>
                                             </button>
-{{--                                        </form>--}}
                                         <script>
                                             function toggleCompleteState() {
                                                 axios.post('{{ route('wish.complete',  $wish->slug) }}')
